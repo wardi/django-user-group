7bf5570715f334b25070a3963079daabdf0b647d
Ian Ward <ian@excess.org>
2007-07-08 16:00:47 +0000

    remove database settings for sandbox (they are in local_settings.py now)
    git-svn-id: http://devel.oclug.on.ca/svn/oclug_django_site/trunk@299 1404dbf3-0cdc-0310-83c5-af7e5224839b

--- sandbox_settings.py
+++ sandbox_settings.py
@@ -3,14 +3,6 @@
 
 from settings import *
 
-
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = '/home/webdude/sandbox/db/db.sqlite'            # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = '/home/webdude/sandbox/site_data/uploaded/'
