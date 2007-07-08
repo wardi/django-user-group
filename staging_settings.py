f6904333b84c87612b33b0aa4b89070fd89993a6
Ian Ward <ian@excess.org>
2007-07-08 02:47:46 +0000

    fix staging TEMPLATE_DIRS, replace invalid "whitesmoke" colour
    git-svn-id: http://devel.oclug.on.ca/svn/oclug_django_site/trunk@297 1404dbf3-0cdc-0310-83c5-af7e5224839b

--- staging_settings.py
+++ staging_settings.py
@@ -6,5 +6,11 @@ from settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/home/webdude/staging/oclug_django_site/templates'
+)
 
 
