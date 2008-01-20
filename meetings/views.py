f4a66d507a6edc785148e0615d165d6be234c665
Ian Ward <ian@excess.org>
2008-01-20 18:21:30 +0000

    make colours bluer (try to match logo better), use red for links, add speaker and location info to meeting detail page, make image view wider, move talk images right
    git-svn-id: http://devel.oclug.on.ca/svn/oclug_django_site/trunk@345 1404dbf3-0cdc-0310-83c5-af7e5224839b

--- meetings/views.py
+++ meetings/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import get_object_or_404, render_to_response
 from models import Image
 
 def view_image(request, path):
-    img = get_object_or_404(Image, src=path)
+    img = get_object_or_404(Image, src="/media/uploaded/"+path)
     return render_to_response('meetings/image_detail.html', {'img':img})
